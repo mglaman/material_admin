f2c0d96cee9df226ba29d0d62b7e92dfab36c03e
Matt Glaman <nmd.matt@gmail.com>
2014-09-10 23:54:12 -0500

    Columnize theme settings form, split columnizer FAPI to a helper.

--- template.php
+++ template.php
@@ -43,6 +43,45 @@ function material_admin_page_alter(&$page) {
  * Customize the update settings form.
  */
 function material_admin_form_update_settings_alter(&$form, &$form_state) {
+  _material_admin_form_split_containers($form);
+
+  // Left
+  $form['admin_container']['admin_left']['update_check_frequency'] = $form['update_check_frequency'];
+  unset($form['update_check_frequency']);
+  $form['admin_container']['admin_left']['update_check_disabled'] = $form['update_check_disabled'];
+  unset($form['update_check_disabled']);
+  // Right
+  $form['admin_container']['admin_right']['update_notify_emails'] = $form['update_notify_emails'];
+  unset($form['update_notify_emails']);
+  $form['admin_container']['admin_right']['update_notification_threshold'] = $form['update_notification_threshold'];
+  unset($form['update_notification_threshold']);
+}
+
+/**
+ * Implements hook_form_alter().
+ */
+function material_admin_form_alter(&$form, &$form_state, $form_id) {
+
+  // Customize theme settings forms (form ID alter is in theme-settings.php.)
+  if ($form_id == 'system_theme_settings') {
+    _material_admin_form_split_containers($form);
+
+    // Left
+    $form['admin_container']['admin_left']['theme_settings'] = $form['theme_settings'];
+    unset($form['theme_settings']);
+
+    // Right
+    $form['admin_container']['admin_right']['logo'] = $form['logo'];
+    unset($form['logo']);
+    $form['admin_container']['admin_right']['favicon'] = $form['favicon'];
+    unset($form['favicon']);
+  }
+}
+
+/**
+ * Helper function for generating left/right form containers.
+ */
+function _material_admin_form_split_containers(&$form) {
   $form['admin_container'] = array(
     '#type' => 'container',
     '#attributes' => array(
@@ -67,15 +106,4 @@ function material_admin_form_update_settings_alter(&$form, &$form_state) {
       ),
     ),
   );
-
-  // Left
-  $form['admin_container']['admin_left']['update_check_frequency'] = $form['update_check_frequency'];
-  unset($form['update_check_frequency']);
-  $form['admin_container']['admin_left']['update_check_disabled'] = $form['update_check_disabled'];
-  unset($form['update_check_disabled']);
-  // Right
-  $form['admin_container']['admin_right']['update_notify_emails'] = $form['update_notify_emails'];
-  unset($form['update_notify_emails']);
-  $form['admin_container']['admin_right']['update_notification_threshold'] = $form['update_notification_threshold'];
-  unset($form['update_notification_threshold']);
 }
