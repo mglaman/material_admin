76c107ccaa8c54f7ca75abc5b1236572fc335497
Matt Glaman <nmd.matt@gmail.com>
2014-09-10 21:23:32 -0500

    Rename theme to Material Design, resolve #2

--- template.php
+++ template.php
@@ -13,7 +13,7 @@ require_once dirname(__FILE__) . '/includes/theme.inc';
 /**
  * Implements hook_css_alter().
  */
-function customer_admin_css_alter(&$css) {
+function material_admin_css_alter(&$css) {
   //dpm($css);
   unset($css['modules/system/system.messages.css']);
   unset($css['modules/system/system.base.css']);
